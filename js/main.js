e5ac79851cf1abd09974ba5dca3d3832a0243454
Willians Júnior <juninhociclistagpi@hotmail.com>
2023-07-24 23:54:20 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required:true 
+            },
+            email: {
+                required:true,
+                email: true 
+            },
+            telefone: {
+                required:false
+            },
+            mensagem: {
+                required:true 
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome!'
+        },
+        submitHandler: function(form) {
+            form.submit();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
+})
